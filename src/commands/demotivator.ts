55a0900f9b453282f4ee69f1272912c311aa5bdf
nik ita <nikita.red638@gmail.com>
2023-05-08 15:55:21 +0400

    demotivator command

---
+++ src/commands/demotivator.ts
@@ -0,0 +1,40 @@
+import { createCanvas, loadImage } from 'canvas';
+import { CommandObject, CommandType } from 'wokcommands';
+import { Props } from '../models';
+import { genFiltered, getRandomImage } from '../utils';
+
+export default {
+  type: CommandType.LEGACY,
+  aliases: ['демотиватор', 'д'],
+  reply: false,
+  callback: async ({ args, guild, message }: Props) => {
+    try {
+      message.channel.sendTyping();
+      const canvas = createCanvas(1280, 1024);
+      const template = await loadImage(
+        'https://cdn.discordapp.com/attachments/829357606224134174/888106080482238495/template.png',
+      );
+      const image = await loadImage(getRandomImage(guild.id));
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(template, 0, 0);
+      ctx.drawImage(image, 118, 103, 1050, 710);
+      ctx.fillStyle = 'white';
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.font = '72px Times New Roman';
+      ctx.fillText(genFiltered(guild.id).slice(0, 30), canvas.width / 2, 886);
+      ctx.font = '40px Times New Roman';
+      ctx.fillText(genFiltered(guild.id).slice(0, 60), canvas.width / 2, 966);
+      return {
+        files: [
+          {
+            attachment: canvas.toBuffer(),
+            name: 'image.png',
+          },
+        ],
+      };
+    } catch (error) {
+      return message.react('❌');
+    }
+  },
+} as CommandObject;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,8 +14,30 @@ export const getMessages = (id: string) => {
   return obj.list as string[];
 };
 
+export const getRandomImage = (id: string) => {
+  const guildName = getGuildName(id);
+  const data = fs.readFileSync(`./src/db/images.${guildName}.json`, 'utf-8');
+  const obj = JSON.parse(data);
+  const images = obj.list.filter(
+    (item) => item.endsWith('.png') || item.endsWith('.jpg') || item.endsWith('.jpeg'),
+  );
+  return images[Math.floor(Math.random() * images.length)];
+};
+
 export const genString = (id: string) => {
   const list = getMessages(id);
+  const max = list.length;
+  const start = Math.floor(Math.random() * max - 1000) + 1;
+  const end = start + 1000;
+  const messages = list.slice(start, end);
+  const markov = new MarkovGen({ input: messages, minLength: 1 });
+  return markov.makeChain();
+};
+
+export const genFiltered = (id: string) => {
+  const list = getMessages(id).filter(
+    (item) => !item.startsWith('<') || !item.endsWith('>') || !item.startsWith('http'),
+  );
   const max = list.length;
   const start = Math.floor(Math.random() * max - 1000) + 1;
   const end = start + 1000;
