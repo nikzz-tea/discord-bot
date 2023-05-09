beefd6cd0e138e7410fecd24053439a3c08375d6
nik ita <nikita.red638@gmail.com>
2023-05-10 01:06:45 +0400

    fix

--- src/events/messageReactionAdd/rolesHandler.ts
+++ src/events/messageReactionAdd/rolesHandler.ts
@@ -9,7 +9,7 @@ export default async (reaction: MessageReaction, user: User) => {
   const sheet = roles[guildName].sheet;
   if (!Object.keys(sheet).includes(reaction.emoji.identifier)) return;
   const role = reaction.message.guild.roles.cache.get(sheet[reaction.emoji.identifier]);
-  const member = reaction.message.guild.members.cache.get(user.id)
+  const member = reaction.message.guild.members.cache.get(user.id);
   member.roles.add(role);
-  logChannel().send(`Gave ${role.name} to ${user.tag}`);
+  logChannel().send(`Gave \`${role.name}\` to \`${user.tag}\``);
 };
