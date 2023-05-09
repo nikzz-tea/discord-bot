25646ffe085f717c76cdbe306a8af85e5cc8bbdd
nikzz <nikita.red638@gmail.com>
2023-05-10 00:41:03 +0400

    fix

--- src/events/messageReactionRemove/rolesRemoveHandler.ts
+++ src/events/messageReactionRemove/rolesRemoveHandler.ts
@@ -1,14 +1,15 @@
-import { MessageReaction } from 'discord.js';
+import { MessageReaction, User } from 'discord.js';
 import { roles } from '../../config.json';
 import { getGuildName, logChannel } from '../../utils';
 
-export default async (reaction: MessageReaction) => {
+export default async (reaction: MessageReaction, user: User) => {
   const guildName = getGuildName(reaction.message.guild.id);
   if (reaction.message.id !== roles[guildName].message) return;
   reaction = await reaction.fetch();
   const sheet = roles[guildName].sheet;
   if (!Object.keys(sheet).includes(reaction.emoji.identifier)) return;
   const role = reaction.message.guild.roles.cache.get(sheet[reaction.emoji.identifier]);
-  reaction.message.member.roles.remove(role);
-  logChannel().send(`Removed ${role.name} from ${reaction.message.member.user.tag}`);
+  const member = reaction.message.guild.members.cache.get(user.id)
+  member.roles.remove(role);
+  logChannel().send(`Removed ${role.name} from ${user.tag}`);
 };
