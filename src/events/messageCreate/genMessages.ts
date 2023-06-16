7f745f17a4b2c7e43814ffe290fc82529d9007ab
nikzz-tea <nikita.red638@gmail.com>
2023-06-16 13:20:01 +0400

    genMessage error fix

--- src/events/messageCreate/genMessages.ts
+++ src/events/messageCreate/genMessages.ts
@@ -4,10 +4,9 @@ import { genString } from '../../utils';
 
 export default (message: Message) => {
   if (message.mentions.has(message.client.user))
-    message.reply(genString(message.guild?.id as string));
-  if (!message.content) return;
+    return message.reply(genString(message.guild?.id as string));
+  if (message.content == '') return;
   if (!message.content.toLowerCase().startsWith(name)) return;
   if (message.content.toLowerCase().startsWith(`${name} кто`)) return;
-  if (message.content.toLowerCase().startsWith(name) && !message.mentions.has(message.client.user))
-    message.channel.send(genString(message.guild?.id as string));
+  message.channel.send(genString(message.guild?.id as string));
 };
