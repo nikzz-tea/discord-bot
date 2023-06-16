d16c3efbb33ab6b68c9e99e787d04969d3d59f4d
nikzz-tea <nikita.red638@gmail.com>
2023-06-16 15:42:41 +0400

    restart command

---
+++ src/commands/restart.ts
@@ -0,0 +1,21 @@
+import { CommandObject, CommandType } from 'wokcommands';
+import { Props } from '../models';
+import { exec } from 'child_process';
+import { logChannel } from '../utils';
+import { ownerId } from '../config.json';
+
+export default {
+  type: CommandType.LEGACY,
+  reply: false,
+  callback: ({ args, guild, message }: Props) => {
+    if (message.author.id !== ownerId) return { content: 'хуй' };
+    if (!args.length) return { content: 'хуй' };
+    exec(`pm2 restart ${args[0]}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      logChannel().send(`\`\`\`${stdout}\`\`\``);
+    });
+  },
+} as CommandObject;
