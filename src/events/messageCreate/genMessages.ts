9c3672a11b580f164d47ec9fc345baa74f3cb122
nikzz-tea <nikita.red638@gmail.com>
2023-06-20 17:03:42 +0400

    genMessage fix(?)

--- src/events/messageCreate/genMessages.ts
+++ src/events/messageCreate/genMessages.ts
@@ -5,7 +5,7 @@ import { genString } from '../../utils';
 export default async (message: Message) => {
   if (message.mentions.has(message.client.user))
     return message.reply(await genString(message.guild?.id as string));
-  if (message.content == '') return;
+  if (message.content == '' || !message.content) return;
   if (!message.content.toLowerCase().startsWith(name)) return;
   if (message.content.toLowerCase().startsWith(`${name} кто`)) return;
   message.channel.send(await genString(message.guild?.id as string));
