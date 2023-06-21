8a49b14f20c399d6e6c211e34b58fae6c12cfa54
nikzz-tea <nikita.red638@gmail.com>
2023-06-21 20:26:29 +0400

    roulette command corrections

--- src/commands/roulette.ts
+++ src/commands/roulette.ts
@@ -1,22 +1,29 @@
 import { CommandObject, CommandType } from 'wokcommands';
 import { Props } from '../models';
+import { logChannel } from '../utils';
 
 export default {
   type: CommandType.LEGACY,
-  aliases: ['рулетка'],
+  aliases: ['рул'],
   reply: false,
   cooldowns: {
-    duration: '1 h',
+    duration: '12 h',
     errorMessage: 'кд',
     type: 'perUserPerGuild',
   },
   callback: async ({ args, guild, message }: Props) => {
+    if (message.member.id === '441628048970743809') return message.react('🦣');
     if (Math.floor(Math.random() * 6) + 1 === 6) {
+      const hour = 1000 * 60 * 60;
+      const timeouts = [hour * 6, hour * 12, hour * 24];
+      const timeoutPhrases = ['6 часов', '12 часов', '**сутки**'];
+      const index = Math.floor(Math.random() * 3) + 1;
       try {
-        await message.member.timeout(1000 * 60 * 60, 'проебал в рулетку');
-        return { content: `${message.member.displayName} отлетает на час` };
+        await message.member.timeout(timeouts[index], 'проебал в рулетку');
+        return { content: `${message.member.displayName} отлетает на ${timeoutPhrases[index]}` };
       } catch (error) {
-        return;
+        logChannel().send('```' + error + '```');
+        return message.react('🦣');
       }
     }
     return { content: 'повезло тебе' };
