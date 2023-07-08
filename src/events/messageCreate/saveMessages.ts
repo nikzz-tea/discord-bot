0e3e1960e4dbbf96379758882899f16550636395
nikzz-tea <nikita.red638@gmail.com>
2023-07-08 16:34:30 +0400

    ignore spoiler attachments

--- src/events/messageCreate/saveMessages.ts
+++ src/events/messageCreate/saveMessages.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { Attachment, Message } from 'discord.js';
 import { name, prefix, saveFromChannels, genPerMessage } from '../../config.json';
 import { genString } from '../../utils';
 import { Images, Messages } from '../../database/models';
@@ -6,21 +6,27 @@ import { Images, Messages } from '../../database/models';
 let count = 0;
 
 export default async (message: Message) => {
+  if (!saveFromChannels.includes(message.channel.id)) return;
+  if (message.author.bot) return;
   if (message.content.startsWith(prefix)) return;
   if (message.content.startsWith(name)) return;
   if (message.content.startsWith(`${name} кто`)) return;
-  if (message.author.bot) return;
-  if (!saveFromChannels.includes(message.channel.id)) return;
   count++;
   count % genPerMessage === 0 &&
     message.channel.send(await genString(message.guild?.id as string, 10));
   const pushItem = (type: string) => {
-    type === 'messages'
-      ? Messages.create({ message: message.content, guildId: message.guildId })
-      : Images.create({
-          image: Array.from(message.attachments.values())[0].url,
+    if (type === 'messages')
+      Messages.create({ message: message.content, guildId: message.guildId });
+    if (type === 'images') {
+      const attachments = Array.from(message.attachments.values());
+      attachments.forEach((attach) => {
+        if (attach.spoiler) return;
+        Images.create({
+          image: attach.url,
           guildId: message.guildId,
         });
+      });
+    }
   };
   if (message.content != '') pushItem('messages');
   if (message.attachments.size > 0) pushItem('images');
