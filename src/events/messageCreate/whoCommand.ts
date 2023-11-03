6cbfef0455aa9c6729a77df0e089c5720b44f227
nikzz-tea <nikita.red638@gmail.com>
2023-11-04 01:35:02 +0300

    whoCommand update

--- src/events/messageCreate/whoCommand.ts
+++ src/events/messageCreate/whoCommand.ts
@@ -1,21 +1,11 @@
 import { Message } from 'discord.js';
-import { name, whoExclude } from '../../config.json';
+import { name } from '../../config.json';
 
 export default async (message: Message) => {
   if (message.author.id === message.client.user?.id) return;
   if (!message.content.toLowerCase().startsWith(`${name} кто`)) return;
-  const members = await message.guild?.members.fetch();
-  const users = [] as string[];
-  members?.forEach((member) => {
-    if (!whoExclude.includes(member.user.username)) users.push(member.user.username);
-  });
-  if (message.content.toLowerCase().startsWith(`${name} кто и кто`)) {
-    message.channel.send(
-      `${users[Math.floor(Math.random() * users.length)]} и ${
-        users[Math.floor(Math.random() * users.length)]
-      }`,
-    );
-    return;
-  }
-  message.channel.send(`я думаю это ${users[Math.floor(Math.random() * users.length)]}`);
+  const members = Array.from(await message.guild?.members.fetch());
+  message.channel.send(
+    `я думаю это ${members[Math.floor(Math.random() * members.length)][1].user.username}`,
+  );
 };
