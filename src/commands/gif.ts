02948c94402e7a9ccf65eaa894ad67a3b94adbbc
nikzz-tea <nikita.red638@gmail.com>
2023-12-08 15:04:23 +0300

    send typing when making gif, handle name of the attachment

--- src/commands/gif.ts
+++ src/commands/gif.ts
@@ -8,10 +8,12 @@ export default {
   callback: async ({ args, guild, message }: Props) => {
     if (message.attachments.size === 0) return message.react('❌');
     try {
+      message.channel.sendTyping();
       const attachment = message.attachments.first();
       const image = await read(attachment.url);
       const buffer = await image.getBufferAsync(MIME_GIF);
-      message.reply({ files: [{ attachment: buffer, name: `${attachment.name}.gif` }] });
+      const name = attachment.name.split('.')[0];
+      message.reply({ files: [{ attachment: buffer, name: `${name}.gif` }] });
     } catch (error) {
       logChannel.send('```' + error + '```');
       message.react('❌');
