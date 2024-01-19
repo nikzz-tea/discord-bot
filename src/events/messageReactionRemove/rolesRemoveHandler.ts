035ef82312493a545a1e67c483091d09bff430b2
nikzz-tea <nikita.red638@gmail.com>
2024-01-19 20:36:07 +0300

    get rid of getGuildName()

--- src/events/messageReactionRemove/rolesRemoveHandler.ts
+++ src/events/messageReactionRemove/rolesRemoveHandler.ts
@@ -1,12 +1,11 @@
 import { MessageReaction, User } from 'discord.js';
 import { roles } from '../../config.json';
-import { getGuildName, logChannel } from '../../utils';
+import { logChannel } from '../../utils';
 
 export default async (reaction: MessageReaction, user: User) => {
-  const guildName = getGuildName(reaction.message.guild.id);
-  if (reaction.message.id !== roles[guildName].message) return;
+  if (reaction.message.id !== roles[reaction.message.guildId].message) return;
   reaction = await reaction.fetch();
-  const sheet = roles[guildName].sheet;
+  const sheet = roles[reaction.message.guildId].sheet;
   if (!Object.keys(sheet).includes(reaction.emoji.identifier)) return;
   const role = reaction.message.guild.roles.cache.get(sheet[reaction.emoji.identifier]);
   const member = reaction.message.guild.members.cache.get(user.id);
