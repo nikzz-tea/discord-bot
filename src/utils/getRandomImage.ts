3a144709782f70bfe2c135d8721c55ced587b72f
nikzz-tea <nikita.red638@gmail.com>
2024-02-27 23:48:33 +0300

    chore: remove console log

--- src/utils/getRandomImage.ts
+++ src/utils/getRandomImage.ts
@@ -3,7 +3,6 @@ import { Images } from '../database/models';
 const getRandomImage = async (id: string) => {
   const table = await Images.findAll({ attributes: ['image'], where: { guildId: id } });
   const images = table.map((image) => image.get('image')) as string[];
-  console.log(images);
   const filtered = images.filter(
     (item) => item.includes('.png') || item.includes('.jpg') || item.includes('.jpeg'),
   );
