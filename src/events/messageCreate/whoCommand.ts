dfd61561049fad6a816b1570828f491c5e8e097a
nikita <nikita.red638@gmail.com>
2025-03-14 17:55:23 +0300

    feat: add role filtering for who command

--- src/events/messageCreate/whoCommand.ts
+++ src/events/messageCreate/whoCommand.ts
@@ -1,11 +1,19 @@
-import { Message } from 'discord.js';
-import { name } from '../../config.json';
+import { GuildMember, Message } from 'discord.js';
+import { name, roleFilter } from '../../config.json';
 
 export default async (message: Message) => {
   if (message.author.id === message.client.user?.id) return;
   if (!message.content.toLowerCase().startsWith(`${name} кто`)) return;
   const members = Array.from(await message.guild?.members.fetch());
-  message.channel.send(
-    `я думаю это ${members[Math.floor(Math.random() * members.length)][1].user.username}`,
+  const sendMessage = (array: [string, GuildMember][]) => {
+    const randomUsername = array[Math.floor(Math.random() * array.length)][1].user.username;
+    message.channel.send('я думаю это ' + randomUsername);
+  };
+  if (!roleFilter[message.guildId]) {
+    return sendMessage(members);
+  }
+  const filteredMembers = members.filter((member) =>
+    member[1].roles.cache.some((role) => roleFilter[message.guildId].includes(role.id)),
   );
+  sendMessage(filteredMembers);
 };
